d58dca13aa6039d46893ee46cc2f0358f6f21008
xiangnan.liu <x22190562@student.ncirl.ie>
2023-08-07 17:39:56 +0100

    add stream response to ControlPanel modual

--- src/main/java/service/ControlPanelServiceImpl.java
+++ src/main/java/service/ControlPanelServiceImpl.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import base.controlPanel.ControlPanelGrpc.ControlPanelImplBase;
+import base.controlPanel.DeviceStatusResponse;
 
 public class ControlPanelServiceImpl extends ControlPanelImplBase {
 
@@ -52,6 +53,24 @@ public class ControlPanelServiceImpl extends ControlPanelImplBase {
 	public void getDeviceStatus(base.controlPanel.DeviceIdentifier request,
 			io.grpc.stub.StreamObserver<base.controlPanel.DeviceStatusResponse> responseObserver) {
 		// asyncUnimplementedUnaryCall(getGetDeviceStatusMethod(), responseObserver);
+		String deviceId = request.getDeviceId();
+		// System.out.println("Request device id is: " + deviceId);
+		for (int i = 0; i < 5; i++) {
+			base.controlPanel.DeviceStatusResponse.Builder builder = base.controlPanel.DeviceStatusResponse.newBuilder();
+			builder.setDeviceId(Integer.toString(i));
+			builder.setStatus(false);
+			base.controlPanel.DeviceStatusResponse build = builder.build();
+			responseObserver.onNext(build);
+			System.out.println("server device id is: " + i);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		responseObserver.onCompleted();
+		
 	}
 
 	/**
